4149e7065f14a4886d87248f35bdfa900fa4147e
blfuentes <epiphonetodie@gmail.com>
2025-12-04 09:31:39 +0100

    finished: 202504 Go

--- AdventOfCode_2025_Go/day04/day04_1.go
+++ AdventOfCode_2025_Go/day04/day04_1.go
@@ -7,11 +7,39 @@ import (
 func Executepart1() int {
 	var result int = 0
 
-	var fileName string = "./day04/test_input_04.txt"
-	// var fileName string = "./day04/day04.txt"
-	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
-		result = len(fileContent)
-	}
+	// var fileName string = "./day04/test_input_04.txt"
+	var fileName string = "./day04/day04.txt"
 
+	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
+		maxRow := len(fileContent) - 1
+		maxCol := len(fileContent[0]) - 1
+		neighbors := [8][2]int{
+			{-1, -1}, {-1, 0}, {-1, 1},
+			{0, -1} /*{0,0},*/, {0, 1},
+			{1, -1}, {1, 0}, {1, 1},
+		}
+		var countPapers = func(r, c int) (counter int) {
+			for _, neighbor := range neighbors {
+				nr := r + neighbor[0]
+				nc := c + neighbor[1]
+				if nr >= 0 && nr <= maxRow && nc >= 0 && nc <= maxCol {
+					if fileContent[nr][nc] == '@' {
+						counter++
+					}
+				}
+			}
+			return
+		}
+
+		for rowIdx, line := range fileContent {
+			for colIdx, char := range line {
+				if char == '@' {
+					if countPapers(rowIdx, colIdx) < 4 {
+						result++
+					}
+				}
+			}
+		}
+	}
 	return result
 }
--- AdventOfCode_2025_Go/day04/day04_2.go
+++ AdventOfCode_2025_Go/day04/day04_2.go
@@ -7,11 +7,62 @@ import (
 func Executepart2() int {
 	var result int = 0
 
-	var fileName string = "./day04/test_input_04.txt"
-	// var fileName string = "./day04/day04.txt"
-	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
-		result = len(fileContent)
-	}
+	// var fileName string = "./day04/test_input_04.txt"
+	var fileName string = "./day04/day04.txt"
 
+	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
+		maxRow := len(fileContent) - 1
+		maxCol := len(fileContent[0]) - 1
+		neighbors := [8][2]int{
+			{-1, -1}, {-1, 0}, {-1, 1},
+			{0, -1} /*{0,0},*/, {0, 1},
+			{1, -1}, {1, 0}, {1, 1},
+		}
+		var countPapers = func(r, c int) (counter int) {
+			for _, neighbor := range neighbors {
+				nr := r + neighbor[0]
+				nc := c + neighbor[1]
+				if nr >= 0 && nr <= maxRow && nc >= 0 && nc <= maxCol {
+					if fileContent[nr][nc] == '@' {
+						counter++
+					}
+				}
+			}
+			return
+		}
+
+		counterMap := make([][]int, 0)
+		var toDelete utilities.Queue[[]int] = utilities.Queue[[]int]{}
+		for rowIdx, line := range fileContent {
+			counterMap = append(counterMap, make([]int, len(line)))
+			for colIdx, char := range line {
+				if char == '@' {
+					counter := countPapers(rowIdx, colIdx)
+					if counter < 4 {
+						toDelete.Enqueue([]int{rowIdx, colIdx})
+					}
+					counterMap[rowIdx][colIdx] = counter
+				} else {
+					counterMap[rowIdx][colIdx] = -1
+				}
+			}
+		}
+		for !toDelete.IsEmpty() {
+			paper, _ := toDelete.Dequeue()
+			result++
+			for _, delta := range neighbors {
+				nr := paper[0] + delta[0]
+				nc := paper[1] + delta[1]
+				if nr >= 0 && nr <= maxRow && nc >= 0 && nc <= maxCol {
+					if counterMap[nr][nc] > 0 {
+						if counterMap[nr][nc] == 4 {
+							toDelete.Enqueue([]int{nr, nc})
+						}
+						counterMap[nr][nc]--
+					}
+				}
+			}
+		}
+	}
 	return result
 }
