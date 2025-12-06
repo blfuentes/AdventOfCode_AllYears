93424487ca3b21030d0a35b08306e8661989228e
blfuentes <epiphonetodie@gmail.com>
2025-12-06 11:35:21 +0100

    refactor: 2025062 Go

--- AdventOfCode_2025_Go/day06/day06_2.go
+++ AdventOfCode_2025_Go/day06/day06_2.go
@@ -10,63 +10,55 @@ func Executepart2() int64 {
 	// var fileName string = "./day06/test_input_06.txt"
 	var fileName string = "./day06/day06.txt"
 	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
-		worksheetIdx := -1
-		worksheets := make([]Worksheet, 0)
-
-		processWorkSheet := func(worksheetIdx int) {
-			var tempResult int64
-			switch worksheets[worksheetIdx].Operation {
-			case '*':
+		processWorkSheet := func(operation rune, numbers []int64) {
+			tempResult := int64(0)
+			if operation == '*' {
 				tempResult = 1
-			case '+':
-				tempResult = 0
 			}
-			for _, number := range worksheets[worksheetIdx].Numbers {
-				switch worksheets[worksheetIdx].Operation {
-				case '*':
+			for _, number := range numbers {
+				if operation == '*' {
 					tempResult *= number
-				case '+':
+				} else {
 					tempResult += number
 				}
 			}
 			result += tempResult
 		}
 
+		var currentNumbers []int64
+		var currentOp rune
 		for wIdx, char := range fileContent[len(fileContent)-1] {
-			if char == '*' || char == '+' || char == '\n' {
-				if worksheetIdx >= 0 {
-					processWorkSheet(worksheetIdx)
+			if char == '*' || char == '+' {
+				if currentNumbers != nil {
+					processWorkSheet(currentOp, currentNumbers)
 				}
-				worksheetIdx++
-				worksheets = append(worksheets, Worksheet{Operation: char, Numbers: make([]int64, 0)})
+				currentOp = char
+				currentNumbers = make([]int64, 0)
 
 				// process the numbers for this worksheet column-wise
-				foundValue := true
-				for cIdx := wIdx; foundValue && cIdx < len(fileContent[len(fileContent)-1]); cIdx++ {
+				for cIdx := wIdx; cIdx < len(fileContent[len(fileContent)-1]); cIdx++ {
 					currentVal := int64(0)
-					foundValue = false
+					hasDigit := false
 					for _, row := range fileContent[:len(fileContent)-1] {
 						tmpChar := rune(row[cIdx])
-						if tmpChar == ' ' {
-							if currentVal > 0 {
-								foundValue = true
-								break
-							}
-						} else {
-							foundValue = true
+						if tmpChar != ' ' {
+							hasDigit = true
 							currentVal = (currentVal * 10) + int64(tmpChar-'0')
+						} else if hasDigit {
+							break
 						}
 					}
-					if foundValue || currentVal > 0 {
-						worksheets[worksheetIdx].Numbers = append(worksheets[worksheetIdx].Numbers, currentVal)
-					} else {
+					if !hasDigit {
 						break
 					}
+					currentNumbers = append(currentNumbers, currentVal)
 				}
 			}
 		}
 		// process the last worksheet
-		processWorkSheet(worksheetIdx)
+		if currentNumbers != nil {
+			processWorkSheet(currentOp, currentNumbers)
+		}
 	}
 
 	return result
