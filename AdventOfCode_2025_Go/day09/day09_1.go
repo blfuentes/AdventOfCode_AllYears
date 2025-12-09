3fa6ae1a6966b2f260280e85498b34557e4cb335
blfuentes <epiphonetodie@gmail.com>
2025-12-09 09:43:43 +0100

    finished: 2025091 Go

--- AdventOfCode_2025_Go/day09/day09_1.go
+++ AdventOfCode_2025_Go/day09/day09_1.go
@@ -1,16 +1,37 @@
 package day09
 
 import (
+	"math"
+	"strings"
+
 	"github.com/blfuentes/AdventOfCode_2025_Go/utilities"
 )
 
-func Executepart1() int {
-	var result int = 0
+func rectangleArea(x1, y1, x2, y2 int64) int64 {
+	return int64((math.Abs(float64(x2-x1)) + 1) * (math.Abs(float64(y2-y1)) + 1))
+}
 
-	var fileName string = "./day09/test_input_09.txt"
-	// var fileName string = "./day09/day09.txt"
-	if fileContent, err := utilities.ReadFileAsText(fileName); err == nil {
-		result = len(fileContent)
+func Executepart1() int64 {
+	var result int64 = 0
+
+	// var fileName string = "./day09/test_input_09.txt"
+	var fileName string = "./day09/day09.txt"
+	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
+
+		for fromIdx := 0; fromIdx < len(fileContent)-1; fromIdx++ {
+			for toIdx := fromIdx + 1; toIdx < len(fileContent); toIdx++ {
+				fX, fY, _ := strings.Cut(fileContent[fromIdx], ",")
+				tX, tY, _ := strings.Cut(fileContent[toIdx], ",")
+				area := rectangleArea(
+					utilities.StringToInt64(fX),
+					utilities.StringToInt64(fY),
+					utilities.StringToInt64(tX),
+					utilities.StringToInt64(tY))
+				if area > result {
+					result = area
+				}
+			}
+		}
 	}
 
 	return result
