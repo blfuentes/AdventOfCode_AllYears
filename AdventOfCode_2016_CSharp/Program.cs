7239b8099045776bf04734f1ad7ea515fb68e6ac
blfuentes <epiphonetodie@gmail.com>
2026-06-13 19:42:45 +0200

    finished: 201609 C#

--- AdventOfCode_2016_CSharp/Program.cs
+++ AdventOfCode_2016_CSharp/Program.cs
@@ -1,4 +1,4 @@
-using AdventOfCode_2016_CSharp;
+﻿using AdventOfCode_2016_CSharp;
 using AdventOfCode_2016_CSharp.day01;
 using AdventOfCode_2016_CSharp.day02;
 using AdventOfCode_2016_CSharp.day03;
@@ -7,6 +7,7 @@ using AdventOfCode_2016_CSharp.day05;
 using AdventOfCode_2016_CSharp.day06;
 using AdventOfCode_2016_CSharp.day07;
 using AdventOfCode_2016_CSharp.day08;
+using AdventOfCode_2016_CSharp.day09;
 using BenchmarkDotNet.Running;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -86,4 +87,12 @@ if (Utils.DownloadContent(client, 2016, 8))
     //BenchmarkRunner.Run<Day08>();
     Console.WriteLine((new Day08(isTest: false)).SolvePart1());
     Console.WriteLine((new Day08(isTest: false)).SolvePart2());
+}
+
+// Day 09
+if (Utils.DownloadContent(client, 2016, 9))
+{
+    //BenchmarkRunner.Run<Day09>();
+    Console.WriteLine((new Day09(isTest: false)).SolvePart1());
+    Console.WriteLine((new Day09(isTest: false)).SolvePart2());
 }
---
+++ AdventOfCode_2016_CSharp/day09/Day09.cs
@@ -0,0 +1,82 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
+using System.Drawing;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdventOfCode_2016_CSharp.day09;
+
+public partial class Day09(bool isTest = false) : BaseDay("09", isTest)
+{
+    #region Part 1
+    [Benchmark]
+    public int RunPart1()
+    {
+        int lengthSoFar = 0;
+        int idx = 0;
+        while (idx < Content.Length)
+        {
+            var match = MarkersRegEx().Match(Content[idx..]);
+            if (match.Success)
+            {
+                (int length, int times) = (int.Parse(match.Value.Split("x")[0][1..]), int.Parse(match.Value.Split("x")[1].Replace(")", "")));
+                lengthSoFar += match.Index + times * length;
+                idx += match.Index + match.Length + length;
+            }
+            else
+            {
+                lengthSoFar += Content[idx..].Length;
+                idx += lengthSoFar;
+            }
+        }
+        if (lengthSoFar == 0) return Content.Length;
+        else return lengthSoFar;
+    }
+
+    public override string SolvePart1()
+    {
+        StopWatch.Start();
+        var result = RunPart1();
+        StopWatch.Stop();
+        return $"Final result Day {Day} part 1: {result} in {Utils.FormatTime(StopWatch.ElapsedTicks)}.";
+    }
+    #endregion
+
+    #region Part 2
+
+    [Benchmark]
+    public long RunPart2()
+    {
+        var toTest = File.ReadAllLines(InputPath);
+        static long GetLength(long currentLength , string input)
+        {
+            var match = MarkersRegEx().Match(input);
+            if (match.Success)
+            {
+                var endIdx = match.Index + match.Value.Length;
+                (int size, long times) = (int.Parse(match.Groups["size"].Value), long.Parse(match.Groups["times"].Value));
+                return currentLength +
+                        (long)match.Index +
+                        times * GetLength(0, input.Substring(endIdx, size)) +
+                        GetLength(0, input[(endIdx + size)..]);
+            }
+            return (long)input.Length;
+        }
+        return GetLength(0, Content);
+    }
+
+    public override string SolvePart2()
+    {
+        StopWatch.Start();
+        var result = RunPart2();
+        StopWatch.Stop();
+        return $"Final result Day {Day} part 2: {result} in {Utils.FormatTime(StopWatch.ElapsedTicks)}.";
+    }
+
+    [GeneratedRegex(@"\((?<size>\d+)x(?<times>\d+)\)")]
+    private static partial Regex MarkersRegEx();
+    #endregion
+}
